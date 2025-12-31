8b659f6aa21214eecf4b62cdcad52f9e5a4e1da5
Irfan Ramadhan <o.irfanramadhan.o@gmail.com>
2025-12-31 12:28:06 +0700

    fix: base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/sejahteraservice/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
